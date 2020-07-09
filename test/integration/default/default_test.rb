0a9c27e3a6cda3e54fc69a091812dd44565f9591
Josh Symes <josh@MacBook-Pro-2020.local>
2020-07-09 19:05:48 +0100

    Add tests for backup cron jobs

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -9,3 +9,19 @@ describe service('bedrock') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe crontab(path: '/etc/cron.d/minecraft-backup') do
+  its('commands') { should include '/opt/minecraftserver/minecraft_backup.sh /opt/minecraftserver/minecraft minecraft.service' }
+  its('minutes') { should cmp '0' }
+  its('hours') { should cmp '04' }
+  its('days') { should cmp '*' }
+  its('user') { should include 'minecraft'}
+end
+
+describe crontab(path: '/etc/cron.d/minecraft-backup-cleanup') do
+  its('commands') { should include '/opt/minecraftserver/minecraft_backup_cleanup.sh' }
+  its('minutes') { should cmp '0' }
+  its('hours') { should cmp '12' }
+  its('days') { should cmp '*' }
+  its('user') { should include 'minecraft'}
+end
